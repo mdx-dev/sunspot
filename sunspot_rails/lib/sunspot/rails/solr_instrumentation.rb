c8fcd1d5ea6255f3eeed534dc9141a0e61174641
Ralph Churchill <ralph.churchill@sapphire-digital.com>
2020-03-24 10:47:22 -0500

    Revert "Using Ruby 2's #prepend replaces alias_method_chain"
    This reverts commit 5872765433c8a352447a05f2422e63db5b90e2c1.

--- sunspot_rails/lib/sunspot/rails/solr_instrumentation.rb
+++ sunspot_rails/lib/sunspot/rails/solr_instrumentation.rb
@@ -4,16 +4,16 @@ module Sunspot
       extend ActiveSupport::Concern
 
       included do
-        prepend(Sunspot::Rails::SolrInstrumentation)
+        alias_method :send_and_receive, :as_instrumentation
       end
 
 
-      def send_and_receive(path, opts)
+      def send_and_receive_with_as_instrumentation(path, opts)
         parameters = (opts[:params] || {})
         parameters.merge!(opts[:data]) if opts[:data].is_a? Hash
         payload = {:path => path, :parameters => parameters}
         ActiveSupport::Notifications.instrument("request.rsolr", payload) do
-          super(path, opts)
+          send_and_receive_without_as_instrumentation(path, opts)
         end
       end
     end
